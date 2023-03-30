fb085e6eafe6d7538286be8697a26d0cc188b53a
Valerii Vasin <valerii.vasin@gmail.com>
2023-03-30 22:06:26 +0200

    feat: extract router config

--- src/components/base/menu/menu.tsx
+++ src/components/base/menu/menu.tsx
@@ -1,22 +1,15 @@
+import { routerConfig } from '../../../router-config';
 import { MenuItem } from './menu-item';
 
 export function Menu() {
   return (
     <div className="pure-menu">
       <ul className="pure-menu-list">
-        <MenuItem pathname="/dragon">Пояс Дракона</MenuItem>
-        <MenuItem pathname="/aden">Кристалл Адена</MenuItem>
-        <MenuItem pathname="/hardin">Кристалл Хардина</MenuItem>
-        <MenuItem pathname="/talisman-of-aden">Талисман Адена</MenuItem>
-        <MenuItem pathname="/talisman-of-eva">Талисман Евы</MenuItem>
-        <MenuItem pathname="/talisman-of-authority">
-          Талисман Властителя
-        </MenuItem>
-        <MenuItem pathname="/hero-crown">Венец Героя</MenuItem>
-        <MenuItem pathname="/talisman-of-benir">Талисман Бенира</MenuItem>
-        <MenuItem pathname="/stones">Драгоценные Камни</MenuItem>
-        <MenuItem pathname="/growth-rune">Руна Развития</MenuItem>
-        <MenuItem pathname="/saiha-cloaks">Плащи Сайхи</MenuItem>
+        {routerConfig.map((item) => (
+          <MenuItem key={item.path} pathname={item.path}>
+            {item.label}
+          </MenuItem>
+        ))}
       </ul>
     </div>
   );
