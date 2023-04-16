f1137909832f558c36af04c0da495fc89160f7c3
Valerii Vasin <valerii.vasin@gmail.com>
2023-04-16 23:46:05 +0200

    feat: dragon belt

--- src/components/agathions.tsx
+++ src/components/agathions.tsx
@@ -1,16 +1,16 @@
 import { EnchantTable } from './base/enchant-table';
 import { Mode } from '../types';
-import { calculateEnchant } from '../helpers/calculate-enchant';
-import { toTableRowProps } from '../helpers/to-table-row-props';
 import { useFormatter } from '../hooks/use-formatter';
+import { useTableRowProps } from '../hooks/use-table-row-props';
 
 const chances = [100, 100, 100, 65, 50, 35, 24, 18, 14, 8];
 
 export function DragonEgg() {
-  const tableRows = toTableRowProps(
-    calculateEnchant(chances, Mode.Destroy),
-    'items',
-  );
+  const tableRows = useTableRowProps({
+    chances,
+    mode: Mode.Destroy,
+    resultMode: 'items',
+  });
   const formatEnchant = useFormatter({ startIndex: 1, type: 'plus' });
   return (
     <EnchantTable
