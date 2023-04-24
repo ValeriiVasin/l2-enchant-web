ff18f4136786e956d8ab069f3da1f7709bfdecb0
Valerii Vasin <valerii.vasin@gmail.com>
2023-04-24 23:13:12 +0200

    feat: refactor tables

--- src/components/dimensional-talisman.tsx
+++ src/components/dimensional-talisman.tsx
@@ -1,51 +1,34 @@
 import { EnchantTable } from './base/enchant-table';
-import { Mode } from '../types';
+import { Mode, TableRowProps } from '../types';
 import { calculateEnchant } from '../helpers/calculate-enchant';
 import { toTableRowProps } from '../helpers/to-table-row-props';
 import { useFormatter } from '../hooks/use-formatter';
 import { adjustTablePropsPrice } from '../helpers/adjust-table-props-price';
 
-function BelowSix() {
-  const chances = [100, 50, 50, 50, 50];
-  const prices = [2, 10, 10, 10, 20];
-  const tableRows = toTableRowProps(
-    calculateEnchant(chances, Mode.Safe),
-    'enchants',
+function prepareRows(): Array<TableRowProps> {
+  const chances = [100, 50, 50, 50, 50, 100, 30, 20, 10, 2];
+  const prices = [2, 10, 10, 10, 20, 0, 8, 8, 8, 8];
+
+  const tableRows = adjustTablePropsPrice(
+    toTableRowProps(calculateEnchant(chances, Mode.Safe), 'enchants'),
+    prices,
   );
 
+  // Lv.6 = Lv.5 x2, adjust Lv.6 and above with the price of Lv.5
+  return tableRows.map((row, index) =>
+    index <= 4 ? row : { ...row, result: row.result + tableRows[4].result },
+  );
+}
+
+export function DimensionalTalisman() {
+  const tableRows = prepareRows();
   const formatEnchant = useFormatter({ startIndex: 1, type: 'level' });
 
   return (
     <EnchantTable
       title="Талисман Иного Измерения"
-      tableRows={adjustTablePropsPrice(tableRows, prices)}
+      tableRows={tableRows}
       formatEnchant={formatEnchant}
     />
-  );
-}
-
-function AboveSix() {
-  const chances = [30, 20, 10, 2];
-  const prices = [8, 8, 8, 8];
-  const tableRows = toTableRowProps(
-    calculateEnchant(chances, Mode.Safe),
-    'enchants',
-  );
-  const formatEnchant = useFormatter({ startIndex: 7, type: 'level' });
-  return (
-    <EnchantTable
-      title="Талисман Иного Измерения"
-      tableRows={adjustTablePropsPrice(tableRows, prices)}
-      formatEnchant={formatEnchant}
-    />
-  );
-}
-
-export function DimensionalTalisman() {
-  return (
-    <div>
-      <BelowSix />
-      <AboveSix />
-    </div>
   );
 }
