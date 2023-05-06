1f0b8d6342e90d2721f3c7670576c44d26b85d23
Valerii Vasin <valerii.vasin@gmail.com>
2023-05-06 22:44:03 +0200

    feat: clan cloak (#59)

---
+++ src/components/clan-cloak.tsx
@@ -0,0 +1,47 @@
+import { useFormatter } from '../hooks/use-formatter';
+import { useTableRowProps } from '../hooks/use-table-row-props';
+import { Mode } from '../types';
+import { EnchantTable } from './base/enchant-table';
+
+const chances = [85, 80, 75, 60, 50, 40, 30, 20, 15, 7];
+
+function Cloaks() {
+  const tableRows = useTableRowProps({
+    chances,
+    mode: Mode.Destroy,
+    resultMode: 'items',
+  });
+  const formatEnchant = useFormatter({ startIndex: 1, type: 'plus' });
+  return (
+    <EnchantTable
+      title="Плащ Клана (Плащи)"
+      tableRows={tableRows}
+      formatEnchant={formatEnchant}
+    />
+  );
+}
+
+function Enchants() {
+  const tableRows = useTableRowProps({
+    chances,
+    mode: Mode.Destroy,
+    resultMode: 'enchants',
+  });
+  const formatEnchant = useFormatter({ startIndex: 1, type: 'plus' });
+  return (
+    <EnchantTable
+      title="Плащ Клана (Заточки)"
+      tableRows={tableRows}
+      formatEnchant={formatEnchant}
+    />
+  );
+}
+
+export function ClanCloak() {
+  return (
+    <>
+      <Cloaks />
+      <Enchants />
+    </>
+  );
+}
