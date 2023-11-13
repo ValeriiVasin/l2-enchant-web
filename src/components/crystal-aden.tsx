ea1ef5051f32cffc829520702dd18fbf65c4eef3
Valerii Vasin <valerii.vasin@gmail.com>
2023-11-13 21:26:21 +0100

    feat: aden armor

--- src/components/crystal-aden.tsx
+++ src/components/crystal-aden.tsx
@@ -3,14 +3,13 @@ import { useTableRowProps } from '../hooks/use-table-row-props';
 import { Mode } from '../types';
 import { EnchantTable } from './base/enchant-table';
 
-const chances: Array<Array<number>> = [
-  [70, 60, 50, 40, 60],
-  [55, 50, 45, 40, 50],
-  [45, 40, 35, 30, 30],
-  [25, 20, 15, 10],
-];
-
-export function CrystalAden() {
+function CrystalAdenWeapon() {
+  const chances: Array<Array<number>> = [
+    [70, 60, 50, 40, 60],
+    [55, 50, 45, 40, 50],
+    [45, 40, 35, 30, 30],
+    [25, 20, 15, 10],
+  ];
   const tableRows = useTableRowProps({
     chances,
     mode: Mode.Drop,
@@ -19,9 +18,40 @@ export function CrystalAden() {
   const formatEnchant = useFormatter({ type: 'level', startIndex: 2 });
   return (
     <EnchantTable
-      title="Кристалл Адена"
+      title="Кристалл Адена - Оружие"
       tableRows={tableRows}
       formatEnchant={formatEnchant}
     />
+  );
+}
+
+function CrystalAdenArmor() {
+  const chances: Array<Array<number>> = [
+    [70, 60, 50, 40, 30],
+    [55, 50, 45, 40, 30],
+    [45, 40, 35, 30, 30],
+    [25, 20, 15, 10],
+  ];
+  const tableRows = useTableRowProps({
+    chances,
+    mode: Mode.Drop,
+    resultMode: 'both',
+  });
+  const formatEnchant = useFormatter({ type: 'level', startIndex: 2 });
+  return (
+    <EnchantTable
+      title="Кристалл Адена - Броня"
+      tableRows={tableRows}
+      formatEnchant={formatEnchant}
+    />
+  );
+}
+
+export function CrystalAden() {
+  return (
+    <div className="pure-g">
+      <CrystalAdenWeapon />
+      <CrystalAdenArmor />
+    </div>
   );
 }
