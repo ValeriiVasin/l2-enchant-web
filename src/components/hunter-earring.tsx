3ed18fe85146dfb49584a13effae568c3725a134
Valerii Vasin <valerii.vasin@gmail.com>
2023-11-13 12:13:55 +0100

    feat: hunter earring (#209)

---
+++ src/components/hunter-earring.tsx
@@ -0,0 +1,22 @@
+import { EnchantTable } from './base/enchant-table';
+import { Mode } from '../types';
+import { useFormatter } from '../hooks/use-formatter';
+import { useTableRowProps } from '../hooks/use-table-row-props';
+
+const chances = [90, 80, 69, 58, 45, 28, 26, 24, 22, 20];
+
+export function HunterEarring() {
+  const tableRows = useTableRowProps({
+    chances,
+    mode: Mode.Destroy,
+    resultMode: 'items',
+  });
+  const formatEnchant = useFormatter({ startIndex: 1, type: 'plus' });
+  return (
+    <EnchantTable
+      title="Серьга Охотника"
+      tableRows={tableRows}
+      formatEnchant={formatEnchant}
+    />
+  );
+}
