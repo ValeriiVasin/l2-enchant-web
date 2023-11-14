48aed5db164a22d6af2d2ae9ae6e93db2fba6f30
Valerii Vasin <valerii.vasin@gmail.com>
2023-11-14 02:11:53 +0100

    refactor: enchant modes

--- src/helpers/calculate-enchant.ts
+++ src/helpers/calculate-enchant.ts
@@ -4,21 +4,81 @@ export function calculateEnchant(
   chances: number[],
   mode = Mode.Drop,
 ): Array<EnchantResult> {
+  switch (mode) {
+    case Mode.Destroy:
+      return convertStrategyResult(modeDestroy(chances));
+    case Mode.Safe:
+      return convertStrategyResult(modeSafe(chances));
+    case Mode.Drop:
+      return convertStrategyResult(modeDrop(chances));
+  }
+}
+
+type EnchantStrategyResult = {
+  chances: Array<number>;
+  enchants: Array<number>;
+  items: Array<number>;
+};
+
+type StrategyFunction = (chances: Array<number>) => EnchantStrategyResult;
+
+const modeDestroy: StrategyFunction = (chances) => {
   const coef = 100 / chances[0];
-  const items = [mode === Mode.Destroy ? coef : 1];
+  const items = [coef];
   const enchants = [coef];
 
   for (let i = 1; i < chances.length; i++) {
     const chance = chances[i];
     const coef = 100 / chance;
 
-    enchants[i] =
-      mode === Mode.Safe
-        ? enchants[i - 1] + coef
-        : enchants[i - 1] * coef + coef;
-    items[i] = mode === Mode.Destroy ? items[i - 1] * coef : 1;
+    enchants[i] = enchants[i - 1] * coef + coef;
+    items[i] = items[i - 1] * coef;
   }
 
+  return { chances, enchants, items };
+};
+
+const modeSafe: StrategyFunction = (chances) => {
+  const coef = 100 / chances[0];
+  const enchants = [coef];
+
+  for (let i = 1; i < chances.length; i++) {
+    const chance = chances[i];
+    const coef = 100 / chance;
+
+    enchants[i] = enchants[i - 1] + coef;
+  }
+
+  return {
+    chances,
+    items: Array(chances.length).fill(1),
+    enchants,
+  };
+};
+
+const modeDrop: StrategyFunction = (chances) => {
+  const coef = 100 / chances[0];
+  const enchants = [coef];
+
+  for (let i = 1; i < chances.length; i++) {
+    const chance = chances[i];
+    const coef = 100 / chance;
+
+    enchants[i] = enchants[i - 1] * coef + coef;
+  }
+
+  return {
+    chances,
+    items: Array(chances.length).fill(1),
+    enchants,
+  };
+};
+
+function convertStrategyResult({
+  enchants,
+  items,
+  chances,
+}: EnchantStrategyResult): Array<EnchantResult> {
   return enchants.map((enchants, i) => ({
     chance: chances[i],
     enchants,
